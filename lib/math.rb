6c8c076b52f9e75c16384339561dc732f2f4b657
Laura Adam <laurawynosky@gmail.com>
2018-05-27 18:26:59 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -35,5 +35,10 @@ num=81
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
-  return __ + (( __ * __ ) / __ )
+  return num1 + (( num2 * num3 ) / num4 )
 end
+num1=7
+num2=43
+num3=23
+num4=83
+
