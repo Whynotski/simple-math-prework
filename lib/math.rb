02a57e26e7b72fced416175431d96e9299db2f0a
Laura Adam <laurawynosky@gmail.com>
2018-05-27 18:20:23 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -13,17 +13,14 @@ num2=5
 def division(num1, num2)
   return num1/num2
 end
-num1=10 
-num2=5 
+num1=50 
+num2=2 
 
 def multiplication(num1, num2)
   return num1*num2
 end
-num1=50 
-num2=2 
-
-num1 = 30
-num2 = 3
+num1=4 
+num2=30 
 
 
 
